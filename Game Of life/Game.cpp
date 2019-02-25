36b39797a173a852cc6d5beb4e077e986dddafd4
Mediev1l <bartek5903@gmail.com>
2019-02-25 11:19:08 +0100

    Nothing has been changed

--- Game Of life/Game.cpp
+++ Game Of life/Game.cpp
@@ -7,6 +7,7 @@ Game::Game()
 void Game::Start()
 {
 	init();
+
 	Game::add_cell(2, 1);
 	Game::add_cell(2, 2);
 	Game::add_cell(2, 3);
@@ -17,6 +18,11 @@ void Game::Start()
 	Game::add_cell(10, 8);
 	Game::add_cell(10, 9);
 	Game::add_cell(10, 10);
+
+	//for (int i = 0; i < (Game::m_width * Game::m_height) / 10; i++)
+	//	Game::add_cell(rand() % Game::get_width(), rand() % Game::get_height());
+
+
 	
 
 	while (1)
