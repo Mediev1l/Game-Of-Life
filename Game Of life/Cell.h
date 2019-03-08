db5b509df40c1105da811d5c342d7f687a0a7cfe
Mediev1l <bartek5903@gmail.com>
2019-03-08 08:49:02 +0100

    Edit

--- Game Of life/Cell.h
+++ Game Of life/Cell.h
@@ -4,24 +4,17 @@
 #ifndef _Cell_H
 #define _Cell_H
 
-#include "Coords.h"
-
 class Cell 
 {
 protected:
 	bool m_Life;
-	int m_lifeTime;
-
 public:
 	Cell();
 
 	void set_Life(bool life);
-	void increment_LifeTime();
-	void reset_LifeTime();
 
 	bool get_Life();
 
-	int get_LifeTime();
 
 	Cell& operator = (const Cell& c);
 };
